162f4d9f1bcb56f9d732be56f0a06a3be0712926
ismaildecencode <ismail@decencode.com>
2023-11-08 21:13:22 +0530

    Added Create Account button Modified launch screen, added text and styles

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Hi Jayabharathi</Text>
+      <Text style={styles.launchScreenText}>MEO MATE</Text>
+      <Pressable>
+        <Text style={styles.creatAccountBtn}
+          onPress={() => {
+            // Add your button click action here
+          }}
+        >Create Account</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  launchScreenText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 60,
+  },
+  creatAccountBtn: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
+    marginTop: 20,
+    width: 200,
+    textAlign: 'center',
+    bottom: -200, 
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
